72031d56bd0120b12e3a54e2fd6ca54033fc7375
brunovsky <brunovskyoliver>
2026-06-02 10:32:20 +0200

    wokring stage

--- __manifest__.py
+++ __manifest__.py
@@ -78,7 +78,6 @@
         "views/contract_tag.xml",
         "views/abstract_contract_line.xml",
         "views/contract.xml",
-        "views/contract_chatter_views.xml",
         "views/contract_supplier_installment_views.xml",
         "views/contract_menu.xml",
         "views/contract_line.xml",
